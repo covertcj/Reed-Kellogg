1e9896182e5ecf4d6f76a7cf1bb7b03430046bc6
Brian Sherman <shermabj@rose-hulman.edu>
2011-05-19 15:54:22 +0000

    Adding password class

---
+++ ReedKellogg/Password.h
@@ -0,0 +1,20 @@
+//
+//  Password.h
+//  ReedKellogg
+//
+//  Created by Prodvend04 on 5/16/11.
+//  Copyright 2011 RHIT. All rights reserved.
+//
+
+#import <CoreData/CoreData.h>
+
+
+@interface Password :  NSManagedObject  
+{
+}
+
+
+@end
+
+
+
---
+++ ReedKellogg/Password.m
@@ -0,0 +1,14 @@
+// 
+//  Password.m
+//  ReedKellogg
+//
+//  Created by Prodvend04 on 5/16/11.
+//  Copyright 2011 RHIT. All rights reserved.
+//
+
+#import "Password.h"
+
+
+@implementation Password 
+
+@end
